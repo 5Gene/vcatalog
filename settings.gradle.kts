4b72a112acbdb015bab6d0d572fe241c97eae54e
ZuYun <jonas.jzy@gmail.com>
2024-09-29 08:09:21 +0800

    📸

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,18 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
+    versionCatalogs {
+        //https://github.com/jamesward/kotlin-universe-catalog
+        // versionCatalogs可以用依赖
+        //  versionCatalogs {
+        //        create("universe") {
+        //            from("com.jamesward.kotlin-universe-catalog:stables:2024.06.12-2")
+        //        }
+        //    }
+        create("wings") {
+            from(files("gradle/libs.versions.toml"))
+        }
+    }
     repositories {
         google()
         mavenCentral()
