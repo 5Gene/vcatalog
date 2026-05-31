039898928c9a18f9c6722625b5454d8e8edbd79e
ZuYun <jonas.jzy@gmail.com>
2026-05-31 13:52:36 +0800

    feat(build): 添加新的插件依赖配置
    - 集成 Kotlin serialization 插件支持序列化功能
    - 添加 Gene Compose 插件用于 Jetpack Compose 开发
    - 配置 Gene Android 插件优化 Android 构建流程
    - 引入 Compose Compiler 插件提升 Compose 性能

--- check/build.gradle.kts
+++ check/build.gradle.kts
@@ -1,7 +1,13 @@
+import org.gradle.kotlin.dsl.vcl
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(vcl.plugins.android.application) apply false
     alias(vcl.plugins.kotlin.jvm) apply false
+    alias(vcl.plugins.kotlin.serialization) apply false
+    alias(vcl.plugins.gene.compose) apply false
+    alias(vcl.plugins.gene.android) apply false
+    alias(vcl.plugins.compose.compiler) apply false
 }
 
 buildscript {
